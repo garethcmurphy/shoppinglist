c5a685ec35481806bbbae2ed51923783d24f63ab
Gareth Murphy <16783958+garethcmurphy@users.noreply.github.com>
2025-01-24 05:44:57 +0100

    🔥 ✨ Refactor App component to use Grid layout and introduce Ingredient interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { DinnerDining, Folder, Menu, MenuBook, Shop2 } from '@mui/icons-material'
-import { AppBar, Avatar, Box, Button, Card, CardContent, CardHeader, CardMedia, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
-import './App.css'
+import { DinnerDining, Folder, Menu, MenuBook, Shop2 } from '@mui/icons-material';
+import {
+  AppBar, Avatar, Box, Button, Card, CardContent,
+  CardHeader, CardMedia, IconButton, List, ListItem, ListItemIcon, ListItemText, Toolbar, Typography
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import './App.css';
 
 function App() {
+
+  interface Ingredient {
+    name: string;
+    quantity: number;
+    unit: string;
+    price: number;
+  };
   const pasta_ingredients = [
     "Pasta",
     "Salt",
@@ -10,7 +21,7 @@ function App() {
     "Tomato Sauce",
   ]
 
-  const ingredient = {
+  const ingredient: Ingredient = {
     name: "Pasta",
     quantity: 1,
     unit: "lb",
@@ -19,13 +30,14 @@ function App() {
 
   const chicken_ingredients = [
     ingredient,
+    {
+      name: "Chicken",
+      quantity: 1,
+      unit: "lb",
+      price: 2.99,
+    }
   ];
 
-  const salmon_ingredients = [
-    "Salmon",
-    "Salt",
-    "Pepper",
-  ]
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -47,56 +59,63 @@ function App() {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
-      <Card className="card" variant="outlined" sx={{ width: 375 }}>
-        <CardHeader 
-          title="Pasta"
-          avatar={<Avatar><MenuBook /></Avatar>}
-          slotProps={{ sx: { width: 40, height: 40, } }}
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image="./src/assets/red-sauce-pasta-1.jpg"
-        >
-        </CardMedia>
-        <CardContent>
 
-          <List>
-            {pasta_ingredients.map((ingredient, index) => (
-              <ListItem key={index}>
-                <ListItemIcon>
-                  <DinnerDining />
-                </ListItemIcon>
-                <ListItemText primary={ingredient} />
-              </ListItem>
-            ))}
-          </List>
-        </CardContent>
-      </Card>
-      <Card className="card" variant="outlined" sx={{ width: 275 }}>
-        <CardHeader
-          avatar={<Avatar><Folder /></Avatar>}
-          title="Shop 2"
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image="./src/assets/red-sauce-pasta-1.jpg"
-        >
-        </CardMedia>
-        <CardContent>
-          <List>
-            {salmon_ingredients.map((ingredient, index) => (
-              <ListItem key={index}>
-                <ListItemIcon>
-                  <Shop2 />
-                </ListItemIcon>
-                <ListItemText primary={ingredient} />
-              </ListItem>
-            ))}
-          </List>
-        </CardContent>
-      </Card>
+      <Grid container spacing={2}>
+        <Grid size={6}>
+          <Card className="card" variant="outlined" >
+            <CardHeader
+              title="Pasta"
+              avatar={<Avatar><MenuBook /></Avatar>}
+              slotProps={{ sx: { width: 40, height: 40, } }}
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image="./src/assets/red-sauce-pasta-1.jpg"
+            >
+            </CardMedia>
+            <CardContent>
+
+              <List>
+                {pasta_ingredients.map((ingredient, index) => (
+                  <ListItem key={index}>
+                    <ListItemIcon>
+                      <DinnerDining />
+                    </ListItemIcon>
+                    <ListItemText primary={ingredient} />
+                  </ListItem>
+                ))}
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid size={6}>
+          <Card className="card" variant="outlined" >
+            <CardHeader
+              avatar={<Avatar><Folder /></Avatar>}
+              title="Shop 2"
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image="./src/assets/red-sauce-pasta-1.jpg"
+            >
+            </CardMedia>
+            <CardContent>
+              <List>
+                {chicken_ingredients.map((ingredient, index) => (
+                  <ListItem key={index}>
+                    <ListItemIcon>
+                      <Shop2 />
+                    </ListItemIcon>
+                    <ListItemText primary={ingredient.name} secondary={ingredient.price} />
+                  </ListItem>
+                ))}
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Box>
   )
 }
